a9bdc5967b66d6607ba8aee69cf0fc9e4f2d2947
MohammadBadir <49198538+MohammadBadir@users.noreply.github.com>
2021-05-01 20:35:04 +0300

    Grade Page progress

--- lib/ui/widgets/grade_calc/grade_calc.dart
+++ lib/ui/widgets/grade_calc/grade_calc.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tasky/app/drawer/navigation_drawer.dart';
@@ -7,15 +8,75 @@ class GradeWidget extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     List temp = [];
-    Provider.of<UserDB>(context).courseGradesMap.forEach((key, value) { temp.add("$key $value");});
+    Provider.of<UserDB>(context).courseGradesMap.forEach((key, value) { temp.add([key,value[0],value[1]]); });
+
+    Widget gradeCardMaker(int index){
+      return Card(
+        color: Colors.white,
+        child: ClipPath(
+          child: Container(
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Text(
+                        temp[index][0],
+                        style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Points: ' + temp[index][1].toString(),
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Grade: ' + temp[index][2].toString(),
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            // height: 100,
+            decoration: BoxDecoration(
+                border: Border(bottom:BorderSide(color: Colors.blueAccent, width: 5) ,top: BorderSide(color: Colors.blueAccent, width: 5))
+            ),
+          )
+          ,
+          clipper: ShapeBorderClipper(shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(3))),
+        ),
+      );
+    }
+    int c;
+    if(MediaQuery.of(context).size.width/4<=150){
+      c=2;
+    } else if(MediaQuery.of(context).size.width/6<=150){
+      c=4;
+    } else if(MediaQuery.of(context).size.width/8<=150){
+      c=6;
+    } else {
+      c=8;
+    }
     return Scaffold(
       appBar: AppBar(title: Center(child: Text("Grades")),),
       drawer: NavigationDrawer(),
       body: Center(
-        child: ListView(
+        child: GridView.count(
+          childAspectRatio: 1,
+          crossAxisCount: c,
           children: List.generate(
               temp.length,
-                  (index) => Text(temp[index])
+                  (index) => gradeCardMaker(index)
           ),
         ),
       ),
@@ -49,7 +110,8 @@ class GradeWidget extends StatelessWidget{
                               labelText: "Points",
                             ),
                             onChanged: (String str){
-                              coursePoints = double.parse(str);
+                              coursePoints = double.tryParse(str);
+                              coursePoints ??= int.tryParse(str)?.toDouble();
                             },
                           ),
                           TextFormField(
@@ -57,7 +119,8 @@ class GradeWidget extends StatelessWidget{
                               labelText: "Grade",
                             ),
                             onChanged: (String str){
-                              courseGrade = double.parse(str);
+                              courseGrade = double.tryParse(str);
+                              courseGrade ??= int.tryParse(str)?.toDouble();
                             },
                           )
                         ],
